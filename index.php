76022720c5caba53a4905164ae0214f5e23b3365
juancarloscr17 <juankcr17@gmail.com>
2021-10-11 23:13:44 -0500

    Ejercicio de constantes y variables

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    header("Content-Type: application/json");
+    define("VELOCIDAD_LUZ",(integer)300000);
+    var_dump(VELOCIDAD_LUZ);
+
+    define("GRAVEDAD",(float)9.8);
+    var_dump(GRAVEDAD) ;
+
+    $MASCOTAS =(string)null;
+    var_dump($MASCOTAS);
+    $MASCOTAS =(string)"gatos";
+    var_dump($MASCOTAS);
+
+
+    
+
+?>
